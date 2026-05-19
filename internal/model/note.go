a58e7888b00f41e6ac0bfb1126a164079df699ae
marathozin <mhozin03@gmail.com>
2026-05-19 22:56:36 +0300

    fix: UserID type

--- internal/model/note.go
+++ internal/model/note.go
@@ -7,7 +7,7 @@ type Note struct {
 	ID        string    `json:"id"`
 	Title     string    `json:"title"`
 	Content   string    `json:"content"`
-	UserID    uint64    `json:"user_id"`
+	UserID    int64     `json:"user_id"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
